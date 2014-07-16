fc383aa225ece5c90f3a17e021bf74190a2035c7
alacambra <alacambra@gmail.com>
2014-07-16 22:32:18 +0200

    dynamic channel creation

--- Entities.py
+++ Entities.py
@@ -1,6 +1,6 @@
 __author__ = 'alacambra'
 import time
-
+import uuid
 
 def get_random_str():
     return get_time_as_str()
@@ -21,14 +21,22 @@ class ChannelItem:
     def __init__(self):
         self.last_ref = ""
         self.counter = 0
+        self.task_gen = Task()
+        self.channel_item_rel_gen = ChannelItemsRelation()
 
-    def get_query(self):
+    def create_channel_item(self):
         item_type = "bce"
         ref = "CI" + str(self.counter)
-        query = "CREATE (" + ref + ":" + item_type + ": " \
-                "{time: " + get_time_as_str() + ", type:'" + item_type + "'})"
+
+        query = "CREATE (" + ref + ":" + item_type + " " \
+                "{time: " + get_time_as_str() + ", type:'" + item_type + "'" \
+                ", uuid:'" + str(uuid.uuid4()) + "'})\n"
+
+        query += self.task_gen.get_query()
+        query += self.channel_item_rel_gen.refer_bce_to_item(ref, self.task_gen.last_ref)
 
         self.last_ref = ref
+        self.counter += 1
         return query
 
 
@@ -37,14 +45,18 @@ class Channel:
     counter = 0
 
     def __init__(self):
+        self.last_ref = ""
         self.counter = 0
 
     def get_query(self):
         ref = "CH" + str(self.counter)
         query = "CREATE (" + ref + ":CHANNEL " \
-                "{time: " + get_time_as_str() + "})"
+                "{time: " + get_time_as_str() + "" \
+                ", uuid:'" + str(uuid.uuid4()) + "'})\n"
 
-        self.counter += self.counter;
+
+        self.counter += 1;
+        self.last_ref = ref
         return query
 
 
@@ -60,12 +72,13 @@ class User:
         ref = "U" + str(self.counter)
         query = "CREATE (" + ref + ":USER " \
                 "{time:" + get_time_as_str() + \
+                ", uuid:'" + str(uuid.uuid4()) + "'" \
                 ", name:'username" + get_random_str() + "'"\
                 ", occupation:'ocupation" + get_random_str() + "'"\
                 ", private:" + str(True).lower() + \
                 ", connectivity:'" + get_connectivity_value(self.counter) + "'" \
                 ", dateModified:" + get_time_as_str() + \
-                ", postalCode:123654})"
+                ", postalCode:123654})\n"
 
         self.last_ref = ref
         self.counter += 1;
@@ -84,7 +97,8 @@ class Task:
         ref = "T" + str(self.counter)
         query = "CREATE (" + ref + ":TASK " + \
                 "{time:" + get_time_as_str() + \
-                ", name: 'task" + get_random_str() + "'"\
+                ", name: 'task" + get_random_str() + "'" \
+                ", uuid:'" + str(uuid.uuid4()) + "'" \
                 ", description:'ipsumipsumipsumipsumipsumipsumipsumipsumipsumipsumipsumipsumipsumipsumipsumipsum'" \
                 ", location:'some place'" \
                 ", private:" + str(True).lower() + \
@@ -92,12 +106,33 @@ class Task:
                 ", dateModified:" + get_time_as_str() + \
                 ", startDate:" + get_time_as_str() + \
                 ", endDate:" + get_time_as_str() + \
-                ", timeRequired:123654})"
+                ", timeRequired:123654})\n"
 
         self.last_ref = ref
         self.counter += 1;
         return query
 
+class ChannelItemsRelation:
+
+    def set_first_item(self, channel_ref, first_item_ref):
+        return "CREATE (" + channel_ref + ")-[:HAS_ITEMS]->(" + first_item_ref + ")\n"
+
+    def set_last_item(self, channel_ref, last_item_ref):
+        return "CREATE (" + last_item_ref + ")-[:LAST]->(" + channel_ref + ")\n"
+
+    def connect_items(self, first_ref, second_ref):
+        return "CREATE (" + first_ref + ")-[:NEXT]->(" + second_ref + ")\n"
+
+    def refer_bce_to_item(self, item_ref, bce_ref):
+        return "CREATE (" + item_ref + ")-[:REFERS_TO]->(" + bce_ref + ")\n"
+
+
+class UserTaskRelation:
+    def set_creator_of_task(self, task_ref, creator_ref):
+        return "CREATE (" + task_ref + ")-[:CREATED_BY]->(" + creator_ref + ")\n"
+
+    def set_assignee_of_task(self, task_ref, assignee_ref):
+        return "CREATE (" + task_ref + ")-[:ASSIGNED_TO]->(" + assignee_ref + ")\n"
 
 if __name__ == "__main__":
 
--- entityloader.py
+++ entityloader.py
@@ -1,10 +1,11 @@
 __author__ = 'alacambra'
 import neo4j
-import time
 import uuid
+import Entities
 
 connection = neo4j.connect("http://localhost:7474")
 cursor = connection.cursor()
+execution = True
 
 def create_nodes(total, type):
 
@@ -30,40 +31,74 @@ def create_nodes(total, type):
     execute(query)
 
 
-def create_channel(total):
+# def create_channel(total):
+#
+#     query = "";
+#     step = 25;
+#
+#     for i in range(0, total):
+#         # if i%step == 0 and i != 0:
+#         #     execute(query)
+#         #     query = ""
+#         # else:
+#         query += "CREATE (n" + str(i) + ":channel" \
+#                  ":UUID {" \
+#                  "privacy:" + str(i%4!=0).lower() + ", " \
+#                                                     "uuid:`" + str(uuid.uuid4()) + "`," \
+#                                                                                    "name:'albert'," \
+#                                                                                    "connectivity:'something'," \
+#                                                                                    "tuPichaLoca:'atributeado'})\n"
+#         if i > 0:
+#             query += "CREATE (n" + str(i-1) + ")-[:NEXT]->(n" + str(i) + ")\n"
+#     execute(query)
 
-    query = "";
-    step = 25;
 
-    for i in range(0, total):
-        # if i%step == 0 and i != 0:
-        #     execute(query)
-        #     query = ""
-        # else:
-        query += "CREATE (n" + str(i) + ":channel" \
-                 ":UUID {" \
-                 "privacy:" + str(i%4!=0).lower() + ", " \
-                                                    "uuid:`" + str(uuid.uuid4()) + "`," \
-                                                                                   "name:'albert'," \
-                                                                                   "connectivity:'something'," \
-                                                                                   "tuPichaLoca:'atributeado'})\n"
-        if i > 0:
-            query += "CREATE (n" + str(i-1) + ")-[:NEXT]->(n" + str(i) + ")\n"
+def create_channel(total_items = 10):
+
+    query = ""
+    channel_gen = Entities.Channel()
+    query += channel_gen.get_query()
+    channel_ref = channel_gen.last_ref
+
+    items_gen = Entities.ChannelItem()
+    query += items_gen.create_channel_item()
+    first_item = items_gen.last_ref
+    channel_items_rel_gen = Entities.ChannelItemsRelation()
+    query += channel_items_rel_gen.set_first_item(channel_ref, first_item)
+
+    second_item = None
+
+    for i in range(0, total_items - 1):
+
+        if second_item is not None:
+            first_item = second_item
+
+        query += items_gen.create_channel_item()
+        second_item = items_gen.last_ref
+
+        query += channel_items_rel_gen.connect_items(first_item, second_item)
+
+    query += channel_items_rel_gen.set_last_item(channel_ref, second_item)
+
     execute(query)
+
 
 
 def clear_all():
     query = "MATCH n, OPTIONAL MATCH n-[r]-m DELETE r, n"
-    execute(query)
-
+    cursor.execute(query)
+    connection.commit()
 
 def execute(query):
-    # cursor.execute(query)
-    # connection.commit()
+    if execution:
+        cursor.execute(query)
+        connection.commit()
+
     print query
 
 
 if __name__ == "__main__":
     print "hello"
     # clear_all();
-    create_channel(2)
+    # create_channel(2)
+    create_channel(25)
